5a29e39a5f42ae004bf2b1d67e9abba7075a1d4b
Lev <ivanovleva512@gmail.com>
2024-04-11 00:05:12 +0700

    M2M Relations completed

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
 
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            dictionary = form.cleaned_data
+            if dictionary.get('is_main'):
+                count += 1
+            else:
+                continue
+        if count == 0:
+            raise ValidationError('Выберите главную тему')
+        elif count > 1:
+            raise ValidationError('Главной темой может быть только одна')
+        return super().clean()
+
+
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [RelationshipInline]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id','name']
